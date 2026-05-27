b37692d02aac8429d38ca3f1c44fe8f5d1e233c2
Sanjay Srinivasan <sanjaysrinivasan810@gmail.com>
2026-05-27 11:49:30 +0530

    Add: min-cost-climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        
+        int first = 0;
+        int second = 0;
+        
+        for(int i=2;i<=n;i++){
+            int temp = min(second + cost[i-1], first + cost[i-2]);
+
+            first = second;
+            second = temp;
+        }
+
+        return second;
+    }
+};
