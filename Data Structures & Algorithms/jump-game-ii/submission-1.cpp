cf1770c113cb9fffb949bf24dd92e5d5df9076a3
Sanjay Srinivasan <sanjaysrinivasan810@gmail.com>
2026-06-27 14:44:16 +0530

    Add: jump-game-ii - submission-1

---
+++ Data Structures & Algorithms/jump-game-ii/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int l = 0; int r = 0;
+        int n = nums.size();
+        int jumps = 0;
+        while(r<n-1){
+            int farthest = 0;
+            for(int i=l; i<=r; i++){
+                farthest = max(farthest, i + nums[i]);
+            }
+            jumps += 1;
+            l = r+1;
+            r = farthest;
+        }
+        return jumps;
+    }
+};
